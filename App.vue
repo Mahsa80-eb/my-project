3da3363162cbcf2bd3aff03711124e098f3001d2
Mahsa80-eb <ebrahimimahsa440@gmail.com>
2025-05-20 12:41:08 +0330

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+   <Header />
+   <Footer />
+  <router-view/>
+</template>
+
+<script>
+import Header from '@/views/Header.vue'
+import Footer from './views/Footer/Footer.vue';
+export default {
+  components: {Header,Footer}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
